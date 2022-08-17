114d6ef27fefb9e86bc14b39c571a00cecd160ba
Brian Drelling <brian@drelling.com>
2022-08-17 02:45:48 -0500

    Added SupportedLanguage extensions

--- Sources/InstrumentKit/Core/Enums/SupportedLanguage.swift
+++ Sources/InstrumentKit/Core/Enums/SupportedLanguage.swift
@@ -4,3 +4,23 @@ public enum SupportedLanguage: String, Codable, Equatable, CaseIterable {
     case english = "en"
     case spanish = "es"
 }
+
+// MARK: - Extensions
+
+extension SupportedLanguage: Identifiable {
+    public var id: RawValue {
+        self.rawValue
+    }
+}
+
+extension SupportedLanguage: CustomStringConvertible {
+    public var description: String {
+        self.rawValue
+    }
+}
+
+extension SupportedLanguage: LosslessStringConvertible {
+    public init?(_ description: String) {
+        self.init(rawValue: description)
+    }
+}
