7970702b28989d46765d012ec542940ea9b4dac6
Brian Drelling <brian@drelling.com>
2023-11-17 19:53:30 -0600

    Made midiNote extension public

--- Sources/NoteKit/Core/Extensions/Note+MIDI.swift
+++ Sources/NoteKit/Core/Extensions/Note+MIDI.swift
@@ -1,6 +1,6 @@
 // Copyright © 2023 Brian Drelling. All rights reserved.
 
-extension Note {
+public extension Note {
     var midiNote: UInt8 {
         UInt8(exactly: (self.octave * PitchClass.allCases.count) + self.pitchClass.rawValue) ?? Note.standard.midiNote
     }
